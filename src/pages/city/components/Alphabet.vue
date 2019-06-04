aa40dca24b4a9c9036f64ab5f8b589aede07c6b4
RicherYY <lansejidiaoyy@outlook.com>
2019-06-04 10:13:32 +0800

    完成城市列表的布局

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .list
+    display flex
+    flex-direction column
+    justify-content center
+    top 1.58rem
+    right 0
+    bottom 0
+    width .4rem
+    position absolute
+    .item
+      color $bgColor
+      text-align center
+      line-height .44rem
+</style>
